1bf08f18c523d469a27b29fa0a0e93aee43bc821
Filipe André da Silva <filipeandre.private@gmail.com>
2022-01-06 09:42:20 -0300

    Implementação de uma api com .NET 6 configurando uma classe Startup.

---
+++ Startup.cs
@@ -0,0 +1,34 @@
+namespace ApiStartup
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
